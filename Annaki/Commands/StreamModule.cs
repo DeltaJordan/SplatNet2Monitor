db933d6beb36c9bac815522e6f6f17fc7fc36a9c
Jordan Marine <zeotni.jordan@gmail.com>
2020-07-11 16:19:01 -0500

    Added the ability to request the notification role.

--- Annaki/Commands/StreamModule.cs
+++ Annaki/Commands/StreamModule.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using Newtonsoft.Json;
 
 namespace Annaki.Commands
 {
     public class StreamModule : BaseModule
     {
-        [Command("bind-stream")]
+        [Command("bind-stream"), RequireOwner]
         public async Task BindStream(CommandContext ctx)
         {
             Globals.BotSettings.StreamNotificationChannelId = ctx.Channel.Id;
@@ -23,6 +25,27 @@ namespace Annaki.Commands
 
             await ctx.Message.DeleteAsync();
             await message.DeleteAsync();
+        }
+
+        [Command("notif")]
+        public async Task ToggleNotifications(CommandContext ctx)
+        {
+            DiscordRole notificationRole = ctx.Guild.Roles.FirstOrDefault(x =>
+                string.Equals(x.Name, "Notifications", StringComparison.InvariantCultureIgnoreCase));
+
+            if (notificationRole == null)
+                return;
+
+            if (ctx.Member.Roles.Any(x => x.Id == notificationRole.Id))
+            {
+                await ctx.Member.RevokeRoleAsync(notificationRole);
+                await ctx.RespondAsync("Removed role successfully.");
+            }
+            else
+            {
+                await ctx.Member.GrantRoleAsync(notificationRole);
+                await ctx.RespondAsync("Added role successfully!");
+            }
         }
 
         protected override void Setup(DiscordClient client)
